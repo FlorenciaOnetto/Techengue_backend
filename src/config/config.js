a08d516a09e373f4fcfe0bc35c9ed699b742ba69
Alexa Bedecarratz <abedecarratz@uc.cl>
2024-12-05 14:54:57 -0300

    render

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -11,7 +11,7 @@ module.exports = {
     logging: false,
   },
   test: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -19,7 +19,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
